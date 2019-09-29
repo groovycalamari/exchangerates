246ba19d03c27940f339129c1b37c430d5b7782e
Sergio del Amo <sergio.delamo@softamo.com>
2019-09-29 04:33:36 +0200

    Fix license

--- subprojects/cli/src/main/java/io/exchangerateapi/cli/CurrencyConverter.java
+++ subprojects/cli/src/main/java/io/exchangerateapi/cli/CurrencyConverter.java
@@ -1,7 +1,7 @@
 /*
  * SPDX-License-Identifier: Apache-2.0
  *
- * Copyright 2019-2020 Sergio del Amo.
+ * Copyright 2019 Sergio del Amo.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
